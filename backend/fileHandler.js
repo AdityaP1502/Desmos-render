f0abb8a956c064e4354df82edd3c2808ee770923
AdityaP1502 <i.m.aditya015@gmail.com>
2022-07-29 04:42:22 +0700

    fileHandler: Add Read Argument function to read argument from argv

--- backend/fileHandler.js
+++ backend/fileHandler.js
@@ -1,20 +1,51 @@
+/* eslint-disable require-jsdoc */
 /* Read Frames Latex Expressions from Files */
 
-const N_PER_BLOCK = 5;
 const fs = require('fs');
-const path = require('path');
 const {EventEmitter} = require('events');
+const {exit} = require('process');
 
-const IN_PATH = path.resolve('out_latex/video1');
-const IMAGES_OUT_PATH = path.resolve('out_png/video1');
+
+let IN_PATH = '';
+let IMAGES_OUT_PATH = '';
+
+let N_PER_BLOCK = 0;
+let FRAME_COUNT = 0;
 
 const myEventEmitter = new EventEmitter();
-
 const framesExprs = [];
+
+class ArgumentError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'ArgumentError';
+  }
+};
+
+const readArgument = () => {
+  try {
+    process.argv.splice(0, 2);
+    if (process.argv.length === 4) {
+      [IN_PATH, IMAGES_OUT_PATH, frameMax, N_PER_BLOCK] = process.argv;
+      FRAME_COUNT += parseInt(frameMax);
+      N_PER_BLOCK = parseInt(N_PER_BLOCK);
+      console.log(`Input path: ${IN_PATH}; Output path: ${IMAGES_OUT_PATH}`);
+      console.log(`Total frames: ${FRAME_COUNT}`);
+      return;
+    }
+    // eslint-disable-next-line max-len
+    throw new ArgumentError('You need to give the necessary arguments when running the server. node ./backend/server.js <in_path> <out_path> <total_frames> <batch_size>');
+  } catch (err) {
+    console.log(err.name);
+    console.log(err.message);
+    exit(1);
+  };
+};
 
 const readNFiles = (start) => {
   console.log(start);
-  for (let i = start; i <= start + N_PER_BLOCK; i++) {
+  // eslint-disable-next-line max-len
+  for (let i = start; i <= start + Math.min(N_PER_BLOCK, (FRAME_COUNT - start)); i++) {
     const exprs = [];
     readFrameExpression(i, exprs);
     // eslint-disable-next-line max-len
@@ -29,9 +60,11 @@ const readFrameExpression = (frame, exprs) => {
 
 const writeImages = (buffer, frame) => {
   console.log(frame);
-  fs.writeFileSync(IMAGES_OUT_PATH + `/Render${frame}.png`, buffer);
+  fs.writeFileSync(IMAGES_OUT_PATH + `Render${frame}.png`, buffer);
 };
 
+// Read Argument
+readArgument();
 // Before starting the server, read the first 100 block of images
 module.exports = {readNFiles, N_PER_BLOCK, framesExprs, myEventEmitter,
-  writeImages};
+  writeImages, FRAME_COUNT};
