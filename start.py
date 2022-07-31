0dc6e7f5c1bee241ccb32f2b1f0e8f8d10397c31
AdityaP1502 <i.m.aditya015@gmail.com>
2022-07-31 22:03:21 +0700

    Add method options

--- start.py
+++ start.py
@@ -1,3 +1,4 @@
+from os import chdir
 from loadingAnimation import Loading, Color
 from preprocess import Preprocess, getcwd, listdir, system
 from processing import Process
@@ -8,13 +9,23 @@ FPS = 24
 N_PER_BATCH = 1000
 
 URL = ""
+METHOD = "canny"
 IN_PATH = ""
 FILENAME = "out"
 FILETYPE = "png"
 OUT_PATH_LATEX = "{}/out_latex/default".format(getcwd())
 OUT_PATH_IMAGES = "{}/out_png/default".format(getcwd())
 
-
+def showInfo():
+  print(Color.print_colored("INPUT_PATH:", utils=["bold"]), end=" ")
+  print(Color.print_colored("{}".format(IN_PATH), color_fg=[10, 20,150]))
+  print(Color.print_colored("OUTPUT_PATH:", utils=["bold"]), end=" ")
+  print(Color.print_colored("{}".format(OUT_PATH_IMAGES), color_fg=[10, 20,150]))
+  print(Color.print_colored("OUTPUT_PATH_LATEX:", utils=["bold"]), end=" ")
+  print(Color.print_colored("{}".format(OUT_PATH_LATEX), color_fg=[10, 20,150]))
+  print(Color.print_colored("METHOD:", utils=["bold"]), end=" ")
+  print(Color.print_colored("{}".format(METHOD), color_fg=[10, 20,150]))
+  
 def showHelp():
   print("""Accepted Arguments:
           -h : show help screen
@@ -25,11 +36,14 @@ def showHelp():
           --path="<frame_path> <out_path_images>": Frame input path. Use relative path. If path specified, latex file will be at "/out_latex/default", change this using --latex option.
           --latex=<out_latex_path>. Use this to specify latex output path when using --path option. 
           --batch_size=<size>: How many frames want to be computed per batch before save to a file. 
+          --edge_method=<edge_detect_method>. Specify the method that want to be used to detect edges. Available option are:    1. canny
+                                                                                                                                2. laplacian
+                                                                                                                                3. combine : combine laplacian and canny method                                                                                                        
           """)
   
 if __name__ == "__main__":
   try:
-    opts, args = getopt.getopt(argv[1:], shortopts="hf:e:", longopts=["fps=", "url=", "path=", "latex=", "batch_size="])
+    opts, args = getopt.getopt(argv[1:], shortopts="hf:e:", longopts=["fps=", "url=", "path=", "latex=", "batch_size=", "edge_method="])
   except getopt.GetoptError as err:
     print(err)
     print("Error : Invalid Argument")
@@ -45,6 +59,8 @@ if __name__ == "__main__":
       
     if opt == "--url":
       URL = arg
+      print(Color.print_colored("URL:", utils=["bold"]), end=" ")
+      print(Color.print_colored("{}".format(URL), color_fg=[10, 20, 150]))
       
     elif opt == "--fps":
       FPS = arg
@@ -53,22 +69,24 @@ if __name__ == "__main__":
       temp = arg.split(" ")
       IN_PATH = getcwd() + temp[0]
       OUT_PATH_IMAGES = getcwd() + temp[1]
-      print(Color.print_colored("INPUT_PATH:", utils=["bold"]), end=" ")
-      print(Color.print_colored("{}".format(IN_PATH), color_fg=[10, 20, 80]))
-      print(Color.print_colored("OUTPUT_PATH:", utils=["bold"]), end=" ")
-      print(Color.print_colored("{}".format(OUT_PATH_IMAGES), color_fg=[10, 20, 80]))
       
-      
+      # make sure out_path_images exist
+      temp = getcwd()
+      Preprocess.changeDir(OUT_PATH_IMAGES)
+      chdir(temp)
+        
     elif opt == "--latex":
       OUT_PATH_LATEX = getcwd() + arg
-      print(Color.print_colored("OUTPUT_PATH_LATEX:", utils=["bold"]), end=" ")
-      print(Color.print_colored("{}".format(OUT_PATH_LATEX), color_fg=[10, 20, 80]))
+      
       
     elif opt == "-f":
       FILENAME = arg
       
     elif opt == "--batch_size":
       N_PER_BATCH = int(arg)
+      
+    elif opt == "--edge_method":
+      METHOD = arg
       
       
   if URL != "":
@@ -78,17 +96,24 @@ if __name__ == "__main__":
     if IN_PATH == "":
       print("Please specified URL or a frame path!")
       exit(1)
+
   
+  showInfo()
   total_frames = len(listdir(IN_PATH))
-  print("Starting processing {} frames".format(total_frames))
+  print(Color.print_colored("Starting ", utils=["bold"]) + "processing" +Color.print_colored(" {} ".format(total_frames), color_fg=[0, 50, 200]) + "frames")
+  
+  proc = Process(IN_PATH, OUT_PATH_LATEX, FILENAME, FILETYPE, N_PER_BATCH, METHOD)
+  try:
+    s = Loading.loading(proc.start)
+    s.join()
+  except Exception as e:
+    print(e)
+    exit(1)
     
-  proc = Process(IN_PATH, OUT_PATH_LATEX, FILENAME, FILETYPE, N_PER_BATCH)
-  Loading.loading(proc.start)
+  # start server with params (in path, out path, total framse)
   print("\rProcess is finished                                                                                                      ")
   print("Running backend server. Please wait!")
-  
-  # start server with params (in path, out path, total framse)
-  cmd = "node ./backend/server.js {} {} {}".format(OUT_PATH_LATEX, OUT_PATH_IMAGES, total_frames)
+  cmd = "node ./backend/server.js {} {} {} {}".format(OUT_PATH_LATEX, OUT_PATH_IMAGES, total_frames, 50)
   system(cmd)
       
     
