23c9549db55fa691fec0822c1daf9cdbe4357173
AdityaP1502 <i.m.aditya015@gmail.com>
2022-07-31 22:05:54 +0700

    fileHandler.js : Add start_frame in arguments

--- backend/fileHandler.js
+++ backend/fileHandler.js
@@ -11,6 +11,9 @@ let IMAGES_OUT_PATH = '';
 
 let N_PER_BLOCK = 0;
 let FRAME_COUNT = 0;
+let N_FRAMES = 0;
+let START_FRAMES = 0;
+
 
 const myEventEmitter = new EventEmitter();
 const framesExprs = [];
@@ -25,12 +28,19 @@ class ArgumentError extends Error {
 const readArgument = () => {
   try {
     process.argv.splice(0, 2);
-    if (process.argv.length === 4) {
-      [IN_PATH, IMAGES_OUT_PATH, frameMax, N_PER_BLOCK] = process.argv;
+    if (process.argv.length >= 4) {
+      [IN_PATH, IMAGES_OUT_PATH, frameMax, N_PER_BLOCK, _] = process.argv;
       FRAME_COUNT += parseInt(frameMax);
       N_PER_BLOCK = parseInt(N_PER_BLOCK);
+
+      // Initialize N_FRAMES
+      N_FRAMES = Math.min(N_PER_BLOCK, (FRAME_COUNT));
+
       console.log(`Input path: ${IN_PATH}; Output path: ${IMAGES_OUT_PATH}`);
       console.log(`Total frames: ${FRAME_COUNT}`);
+
+      if (_ !== undefined) START_FRAMES = _;
+      console.log(`Start Frames: ${START_FRAMES}`);
       return;
     }
     // eslint-disable-next-line max-len
@@ -43,9 +53,10 @@ const readArgument = () => {
 };
 
 const readNFiles = (start) => {
-  console.log(start);
+  N_FRAMES = Math.min(N_PER_BLOCK, (FRAME_COUNT - start + 1));
+
   // eslint-disable-next-line max-len
-  for (let i = start; i <= start + Math.min(N_PER_BLOCK, (FRAME_COUNT - start)); i++) {
+  for (let i = start; i < start + N_FRAMES; i++) {
     const exprs = [];
     readFrameExpression(i, exprs);
     // eslint-disable-next-line max-len
@@ -60,11 +71,12 @@ const readFrameExpression = (frame, exprs) => {
 
 const writeImages = (buffer, frame) => {
   console.log(frame);
-  fs.writeFileSync(IMAGES_OUT_PATH + `Render${frame}.png`, buffer);
+  fs.writeFileSync(IMAGES_OUT_PATH + `/Render${frame}.png`, buffer);
 };
 
 // Read Argument
 readArgument();
+
 // Before starting the server, read the first 100 block of images
-module.exports = {readNFiles, N_PER_BLOCK, framesExprs, myEventEmitter,
-  writeImages, FRAME_COUNT};
+module.exports = {readNFiles, N_PER_BLOCK, N_FRAMES,
+  framesExprs, myEventEmitter, writeImages, FRAME_COUNT, START_FRAMES};
